17d98a900e4d5b0d723a9ec6172fe1a78a4f031a
Muhammad Islah Uddin <islah.uddin.56829@gmail.com>
2025-09-17 19:31:54 +0500

    all work

---
+++ day7_bash_scripting/basics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Variables
+name="Islah"
+echo "My name is $name"
+
+# If condition
+if [ $name == "Islah" ]; then
+  echo "Welcome, DevOps Engineer!"
+else
+  echo "Access Denied"
+fi
+
+# Loop
+for i in 1 2 3 4 5
+do
+  echo "Looping number: $i"
+done
+
